5569cd43716b931a76b9ae62c234711abdffb8e2
Julich Mera <mera7617@gmail.com>
2016-05-11 19:02:36 -0400

    Update deploy.js Include development target. It isn't working fully right now but it is a WIP. Right now highest priority is getting this initial release out.

--- config/deploy.js
+++ config/deploy.js
@@ -1,5 +1,5 @@
 /* jshint node: true */
-var VALID_DEPLOY_TARGETS = [ 'staging', 'production' ];
+var VALID_DEPLOY_TARGETS = [ 'development', 'staging', 'production' ];
 
 module.exports = function(deployTarget) {
   if (VALID_DEPLOY_TARGETS.indexOf(deployTarget) === -1) {
@@ -10,12 +10,11 @@ module.exports = function(deployTarget) {
     build: {
       environment: deployTarget
     },
-    redis: {
+    'redis': {
       allowOverwrite: true,
-      keyPrefix: 'client:index',
-
+      keyPrefix: 'client:index'
     },
-    revisionData: {
+    'revision-data': {
       type: 'git-commit'
     },
     s3: {
@@ -30,14 +29,21 @@ module.exports = function(deployTarget) {
     // include other plugin configuration that applies to all deploy targets here
   };
 
+  if (deployTarget === 'development') {
+    // TODO right now this doesn't seem to be the right config to deploy
+    // index.html to local redis store
+    ENV.plugins = ['build', 'redis', 'revision-data', 'display-revisions', 'redis'];
+    ENV['redis'].url = 'redis://localhost:6379/';
+  }
+
   if (deployTarget === 'staging') {
-    ENV['ssh-tunnel'].dstHost = "api-staging.zhlz3v.0001.usw2.cache.amazonaws.com";
-    // ENV.redis.url = 'redis://api-staging.zhlz3v.0001.usw2.cache.amazonaws.com:6739';
     ENV.s3.bucket = 'staging.rentirooms.com';
+    ENV['ssh-tunnel'].dstHost = "api-staging.zhlz3v.0001.usw2.cache.amazonaws.com";
   }
 
   if (deployTarget === 'production') {
     ENV.s3.bucket = 'production.rentirooms.com';
+    ENV['ssh-tunnel'].dstHost = "api-production.zhlz3v.0001.usw2.cache.amazonaws.com";
   }
 
   // Note: if you need to build some configuration asynchronously, you can return
