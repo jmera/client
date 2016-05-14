5ee8d84349cc9e59be6f9e9877c83824cc2c232f
Julich Mera <mera7617@gmail.com>
2016-05-14 02:31:58 -0400

    Don't prepopulate for data I no longer need this to debug

--- app/routes/i-have-a-room.js
+++ app/routes/i-have-a-room.js
@@ -3,44 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
-      room: this.store.createRecord('room', {
-        furnished: true,
-        bed: true,
-        rug: true,
-        mirror: true,
-        nightDesk: false,
-        tv: false,
-        drawer: true,
-        closet: true,
-        cable: false,
-        bathroom: true,
-        wifi: false,
-        electricity: true,
-        radio: true,
-        visitorsAllowed: true,
-        petsAllowed: true,
-        sharedLivingRoom: true,
-        sharedBathroom: true,
-        sharedKitchen: true,
-        resides: true,
-        rentType: 1,
-        other: 'Hot potatoe',
-        rentAmount: '150.00',
-        visitorPolicy: 'Only immediate family'
-      }),
-      user: this.store.createRecord('user', {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'john@doe.com',
-        phoneNumber: '231-000-9999'
-      }),
-      address: this.store.createRecord('address', {
-        line1: '100 Wood Rd',
-        apt: '1',
-        city: 'New York',
-        state: 'NY',
-        zipCode: '10001'
-      })
+      room: this.store.createRecord('room'),
+      user: this.store.createRecord('user'),
+      address: this.store.createRecord('address')
     });
   },
   actions: {
@@ -49,7 +14,14 @@ export default Ember.Route.extend({
       var room = controller.get('room');
       room.set('user', controller.get('user'));
       room.set('address', controller.get('address'));
-      room.save();
+      var self = this;
+      room.save().then(function() {
+        alert("Success!");
+        self.transitionTo('/');
+      }, function() {
+        alert("There was a problem completing your request.");
+        self.transitionTo('/');
+      });
     }
   },
   setupController(controller, model) {
